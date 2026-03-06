8d8406ca3e597acc8fdf5e8ebca4efba04bb829f
LDT9510 <luis.ernesto951008@gmail.com>
2026-03-06 09:05:11 -0300

    Fix completion in various contexts
    This is a port of this unmerged PR: https://github.com/godotengine/godot/pull/102937 (as of 2026-03-06)

--- modules/gdscript/gdscript_editor.cpp
+++ modules/gdscript/gdscript_editor.cpp
@@ -3464,6 +3464,7 @@ static void _find_call_arguments(GDScriptParser::CompletionContext &p_context, c
 
 	switch (completion_context.type) {
 		case GDScriptParser::COMPLETION_NONE:
+		case GDScriptParser::COMPLETION_DECLARATION:
 			break;
 		case GDScriptParser::COMPLETION_ANNOTATION: {
 			List<MethodInfo> annotations;
@@ -4411,6 +4412,8 @@ static Error _lookup_symbol_from_base(const GDScriptParser::DataType &p_base, co
 		case GDScriptParser::COMPLETION_METHOD:
 		case GDScriptParser::COMPLETION_ASSIGN:
 		case GDScriptParser::COMPLETION_CALL_ARGUMENTS:
+		case GDScriptParser::COMPLETION_DECLARATION:
+		case GDScriptParser::COMPLETION_INHERIT_TYPE:
 		case GDScriptParser::COMPLETION_IDENTIFIER:
 		case GDScriptParser::COMPLETION_PROPERTY_METHOD:
 		case GDScriptParser::COMPLETION_SUBSCRIPT: {
@@ -4603,8 +4606,14 @@ static Error _lookup_symbol_from_base(const GDScriptParser::DataType &p_base, co
 			}
 		} break;
 		case GDScriptParser::COMPLETION_OVERRIDE_METHOD: {
+			// This logic applies to any method declaration (override or not),
+			// but shows parent documentation on virtual method overrides.
 			GDScriptParser::DataType base_type = context.current_class->base_type;
+			if (_lookup_symbol_from_base(base_type, p_symbol, r_result) == OK) {
+				return OK;
+			}
 
+			base_type = context.current_class->get_datatype();
 			if (_lookup_symbol_from_base(base_type, p_symbol, r_result) == OK) {
 				return OK;
 			}
@@ -4617,6 +4626,21 @@ static Error _lookup_symbol_from_base(const GDScriptParser::DataType &p_base, co
 			if (_lookup_symbol_from_base(base_type, p_symbol, r_result) == OK) {
 				return OK;
 			}
+			
+			if (ScriptServer::is_global_class(p_symbol)) {
+				const String scr_path = ScriptServer::get_global_class_path(p_symbol);
+				const Ref<Script> scr = ResourceLoader::load(scr_path);
+				if (scr.is_null()) {
+					return ERR_BUG;
+				}
+				r_result.type = ScriptLanguage::LOOKUP_RESULT_CLASS;
+				r_result.class_name = scr->get_doc_class_name();
+				r_result.script = scr;
+				r_result.script_path = scr_path;
+				r_result.location = 0;
+				return OK;
+			}
+			
 		} break;
 		case GDScriptParser::COMPLETION_ANNOTATION: {
 			const String annotation_symbol = "@" + p_symbol;
